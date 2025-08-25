519fb6f7ba19cd822389404dd1ea349b87cb43c9
RoboBoost <125383681+RoboBoost@users.noreply.github.com>
2025-08-25 23:01:06 +0300

    Create blocks.js

---
+++ blocks.js
@@ -0,0 +1,20 @@
+Blockly.defineBlocksWithJsonArray([
+  {
+    "type": "led_on",
+    "message0": "увімкнути світлодіод",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 120,
+    "tooltip": "Увімкнути світлодіод ESP32",
+    "helpUrl": ""
+  },
+  {
+    "type": "led_off",
+    "message0": "вимкнути світлодіод",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 120,
+    "tooltip": "Вимкнути світлодіод ESP32",
+    "helpUrl": ""
+  }
+]);
