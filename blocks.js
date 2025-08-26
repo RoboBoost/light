c970b8bc69085ea1363f347a3faf739e61f72a58
RoboBoost <125383681+RoboBoost@users.noreply.github.com>
2025-08-27 00:10:31 +0300

    Update blocks.js

--- blocks.js
+++ blocks.js
@@ -1,20 +1,51 @@
 Blockly.defineBlocksWithJsonArray([
   {
-    "type": "led_on",
-    "message0": "увімкнути світлодіод",
+    "type": "funny_led_on",
+    "message0": "😁 ЗАСЯЙ ЯСКРАВО %1",
+    "args0": [
+      {
+        "type": "field_image",
+        "src": "https://img.icons8.com/color/48/000000/light-on.png",
+        "width": 32,
+        "height": 32,
+        "alt": "Лампочка"
+      }
+    ],
     "previousStatement": null,
     "nextStatement": null,
-    "colour": 120,
-    "tooltip": "Увімкнути світлодіод ESP32",
+    "colour": 60,
+    "tooltip": "Вмикає веселу лампочку!",
     "helpUrl": ""
   },
   {
-    "type": "led_off",
-    "message0": "вимкнути світлодіод",
+    "type": "funny_led_off",
+    "message0": "🛏️ ТИХО ЛАМПОЧКА %1",
+    "args0": [
+      {
+        "type": "field_image",
+        "src": "https://img.icons8.com/color/48/000000/light-off.png",
+        "width": 32,
+        "height": 32,
+        "alt": "Гасити світло"
+      }
+    ],
     "previousStatement": null,
     "nextStatement": null,
-    "colour": 120,
-    "tooltip": "Вимкнути світлодіод ESP32",
+    "colour": 210,
+    "tooltip": "Гасить лампочку",
     "helpUrl": ""
-  }
-]);
+  },
+  {
+    "type": "funny_colour_led",
+    "message0": "🎨 РОЗМАЛЮЙ ЛАМПОЧКУ у %1",
+    "args0": [
+      {
+        "type": "field_colour",
+        "name": "COLOR",
+        "colour": "#ff00ff"
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 330,
+    "tooltip": "Лампочка змінить кол
